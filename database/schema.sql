553e43b45caff10feaaee8538d5f9f4e9299e129
vinifen <vinifen@users.noreply.github.com>
2025-05-10 19:50:25 -0300

    📝 Update schema db user name

--- database/schema.sql
+++ database/schema.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS users, events, users_events, participants;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     encrypted_password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
