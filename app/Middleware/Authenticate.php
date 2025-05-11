2aa89a4ce4a252d6db16fdda793e14cb397d5e6d
vinifen <ferreiranivicius@gmail.com>
2025-05-11 00:23:20 -0300

    🔖 First branch commit

--- app/Middleware/Authenticate.php
+++ app/Middleware/Authenticate.php
@@ -12,7 +12,7 @@ class Authenticate implements Middleware
     public function handle(Request $request): void
     {
         if (!Auth::check()) {
-            FlashMessage::danger('Você deve estar logado para acessar essa página');
+            FlashMessage::danger('Você deve estar logado para acessar essa página.');
             $this->redirectTo(route('users.login'));
         }
     }
