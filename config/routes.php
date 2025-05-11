eb03010d5abd4753d2e66c7f72d95d39f2e2422a
vinifen <vinifen@users.noreply.github.com>
2025-05-10 23:01:16 -0300

    ✨ Add admin, login & landing pages

--- config/routes.php
+++ config/routes.php
@@ -1,7 +1,20 @@
 <?php
 
 use App\Controllers\HomeController;
+use App\Controller\AuthController;
 use Core\Router\Route;
 
 // Authentication
 Route::get('/', [HomeController::class, 'index'])->name('root');
+
+Route::get('/', [WelcomeController::class, 'login'])->name('login');
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('/admin', [AdminController::class, 'login'])->name('login');
+
+
+//rota post do login
+//rota post do logout
+//rota admin get
+//rota usuario normal get
